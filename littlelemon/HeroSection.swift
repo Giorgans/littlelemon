63142c65aff1b8e2932ef0c229d7fb471e3bcc97
George Zervos <g.zervos96@windowslive.com>
2023-09-22 15:06:26 +0300

    implemented the hero section

---
+++ littlelemon/HeroSection.swift
@@ -0,0 +1,18 @@
+//
+//  HeroSection.swift
+//  littlelemon
+//
+//  Created by Georgios Zervos on 22/9/23.
+//
+
+import SwiftUI
+
+struct HeroSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroSection()
+}
