9dd08fec47b93b2de4d926498f1ac723918fe518
George Zervos <g.zervos96@windowslive.com>
2023-09-23 01:10:24 +0300

    done some changes

---
+++ littlelemon/MenuBreakdown.swift
@@ -0,0 +1,18 @@
+//
+//  MenuBreakdown.swift
+//  littlelemon
+//
+//  Created by Georgios Zervos on 22/9/23.
+//
+
+import SwiftUI
+
+struct MenuBreakdown: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuBreakdown()
+}
---
+++ littlelemon/ProfilePicture.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePicture.swift
+//  littlelemon
+//
+//  Created by Georgios Zervos on 23/9/23.
+//
+
+import SwiftUI
+
+struct ProfilePicture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePicture()
+}
